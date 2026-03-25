faf69ec8ded60839ff754d218c30f20cdeeaca77
Giulliano Guimarães <llthguimaraes@gmail.com>
2026-03-25 12:21:30 -0300

    [FIX] - Ajuste na função de barra de progresso

--- assets/script/progress-bar.js
+++ assets/script/progress-bar.js
@@ -1,15 +1,14 @@
 const scrollBar = document.getElementById('scrollBar');
 
-// Quanto o usuário já desceu
-const windowScroll = document.documentElement.scrollTop;
-
-// Altura total rolável (Altura total - Altura da tela visível)
-const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-// Cálculo da porcentagem
-const scrolled = (windowScroll / height) * 100;
-
 export default function ProgressBar() {
+    // Quanto o usuário já desceu
+    const windowScroll = document.documentElement.scrollTop;
+
+    // Altura total rolável (Altura total - Altura da tela visível)
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    // Cálculo da porcentagem
+    const scrolled = (windowScroll / height) * 100;
     // Aplica a largura no CSS
     if (scrollBar) {
         scrollBar.style.width = scrolled + "%";
