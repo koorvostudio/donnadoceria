a4766ed0405dc1272e3e6449c0badca23a73773d
Giulliano Guimarães <llthguimaraes@gmail.com>
2026-03-25 16:35:10 -0300

    [FIX] - Ajustes no evento de abertura do menu

--- assets/script/header.js
+++ assets/script/header.js
@@ -5,12 +5,24 @@ const links = document.querySelectorAll('.menu-drawer a');
 
 
 const toggleMenu = () => {
-    drawer.classList.toggle('active');
-    overlay.classList.toggle('active');
-    openBtn.classList.toggle('active'); // Adiciona/remove a classe no botão para o efeito do X
+    if (window.innerWidth <= 992) {
+        drawer.classList.toggle('active');
+        overlay.classList.toggle('active');
+        openBtn.classList.toggle('active'); // Adiciona/remove a classe no botão para o efeito do X
 
-    const isActive = drawer.classList.contains('active');
-    document.body.style.overflow = isActive ? 'hidden' : 'auto';
+        const isActive = drawer.classList.contains('active');
+        document.body.style.overflow = isActive ? 'hidden' : 'auto';
+    }
+};
+
+export const RedefineMenu = () => {
+    if (window.innerWidth > 992) {
+        drawer.classList.remove('active');
+        overlay.classList.remove('active');
+        openBtn.classList.remove('active');
+
+        document.body.style.overflow = "auto";
+    }
 };
 
 export default function Header() {
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 import { FloatingWhatsAppButton, ShowOnApproachWhatsAppButton, ShowOrHideWhatsAppButton } from "./floating-button.js";
-import Header from "./header.js";
+import Header, { RedefineMenu } from "./header.js";
 import ChangeMap from "./map.js";
 import ProgressBar from "./progress-bar.js";
 import Reveal from "./reveal.js";
@@ -17,4 +17,8 @@ window.addEventListener("scroll", () => {
 
 document.addEventListener('mousemove', (e) => {
     ShowOnApproachWhatsAppButton(e.clientX, e.clientY);
+});
+
+window.addEventListener("resize", () => {
+    RedefineMenu();
 });
