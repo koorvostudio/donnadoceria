258c1cb7143b958d641024aa65fedea5e3d2b13f
Giulliano Guimarães <llthguimaraes@gmail.com>
2026-03-25 12:19:24 -0300

    [FEAT] - Eventos de revelar o botão flutuante do Wpp

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+import { FloatingWhatsAppButton, ShowOnApproachWhatsAppButton, ShowOrHideWhatsAppButton } from "./floating-button.js";
 import Header from "./header.js";
 import ChangeMap from "./map.js";
 import ProgressBar from "./progress-bar.js";
@@ -7,7 +8,13 @@ Header();
 ChangeMap();
 Reveal();
 ProgressBar();
+FloatingWhatsAppButton();
 
 window.addEventListener("scroll", () => {
     ProgressBar();
+    ShowOrHideWhatsAppButton();
+});
+
+document.addEventListener('mousemove', (e) => {
+    ShowOnApproachWhatsAppButton(e.clientX, e.clientY);
 });
